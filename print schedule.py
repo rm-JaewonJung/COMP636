baab8582a5c8f93cc64bff238d9f08e6ce3099ea
Jaewon Jung <jaewon.jung2@lincolnuni.ac.nz>
2024-07-16 22:40:53 +1200

    v1

---
+++ print schedule.py
@@ -0,0 +1,11 @@
+def print_schedule():
+    print("Learning sessions:")
+    print('  ''Tuesdays: 9:00am - 10:30am')
+    print("  Thursdays: 11:00am - 12:30pm")
+    print("Support sessions:")
+    print("  Mondays: 4:30pm - 6:00pm")
+    print("  Thursdays: 4:30pm - 6:00pm")
+
+if __name__ == "__main__":
+    print("My schedule:")
+    print_schedule()
